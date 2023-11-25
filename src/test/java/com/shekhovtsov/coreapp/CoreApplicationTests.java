849f2d26a245a0d2fb630f5e458ec0b5f501d67b
shekhovtsovsergey <89629417575@mail.ru>
2023-11-25 18:42:03 +0300

    Initial commit

---
+++ src/test/java/com/shekhovtsov/coreapp/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shekhovtsov.coreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
